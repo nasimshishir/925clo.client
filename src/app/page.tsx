b6b2c29d75c76d38b0f5d8811aacc85f50029fe4
Md Nisat Us Sakib <skb.sagar17@gmail.com>
2023-08-05 16:41:29 +0600

    build style profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Herov1 } from '@components'
-import { useDropdownMenuContext } from '@lib/context api/providers/DropdownMenuContextProvider';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
+import mobile from '@assets/mobile_section.webp'
 
 
 export default function Home() {
@@ -12,8 +13,22 @@ export default function Home() {
   }
 
   return (
-    <main className='min-h-screen'>
-      <Herov1 />
+    <main className='min-h-screen absolute w-full'>
+      <section>
+        <Herov1 />
+      </section>
+      <section className='bg-primary_orange'>
+        <div className='grid grid-cols-2'>
+          <div className=''>
+            <h2 className='text-white sec_title mb-2'>Build Style Profile</h2>
+            <p className='text-white sec_desc mb-5'>In this style profile we ask for your preferences on brands, item types and colours to help us learn more about you and your individual style.
+            </p>
+          </div>
+          <div className='relative'>
+            <Image className='' src={mobile} alt='testing' fill />
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
--- src/components/Hero.v1.tsx
+++ src/components/Hero.v1.tsx
@@ -13,20 +13,18 @@ const Herov1: React.FC = () => {
 
 
     return (
-        <section className='min-h-screen absolute w-full bg-cover bg-center' style={{ backgroundImage: 'url(https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg)' }}>
-            <div className='relative h-screen max-h-screen w-full'>
-                {/* <Image className='-z-10 object-cover content-center' src={bg} alt='testing' fill /> */}
-                <div className='flex flex-col justify-center items-center min-h-screen'>
-                    <h2 className='text-white sec_title mb-2'>Build Style Profile</h2>
-                    <p className='text-white sec_desc text-center mb-5'>In this style profile we ask for your preferences on brands, item types and colours to help us learn more about you and your individual style.
-                    </p>
-                    <div className='mt-10'>
-                        <Buttonv1 type='submit' css='mb-3' to='/auth/login'>Log in</Buttonv1>
-                        <Buttonv2 type='submit' to='/auth/signup'>Sign Up</Buttonv2>
-                    </div>
+        <div className='bg-cover bg-center' style={{ backgroundImage: 'url(https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg)' }}>
+            {/* <Image className='-z-10 object-cover content-center' src={bg} alt='testing' fill /> */}
+            <div className='flex flex-col justify-center items-center min-h-screen'>
+                <h2 className='text-white sec_title mb-2'>Build Style Profile</h2>
+                <p className='text-white sec_desc text-center mb-5'>In this style profile we ask for your preferences on brands, item types and colours to help us learn more about you and your individual style.
+                </p>
+                <div className='mt-10'>
+                    <Buttonv1 type='submit' css='mb-3' to='/auth/login'>Log in</Buttonv1>
+                    <Buttonv2 type='submit' to='/auth/signup'>Sign Up</Buttonv2>
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
